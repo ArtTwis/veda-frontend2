4157fc2743bcf30fa3d0aea7421ad2df08d41c5c
Art_Twis <34947496+ArtTwis@users.noreply.github.com>
2024-08-31 04:01:56 +0530

    implement react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import App from "./App.jsx";
+import PageNotFound from "./components/PageNotFound/index.jsx";
+import SignIn from "./views/SignIn/index.jsx";
+import "./index.css";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: "BodyElement",
+      },
+      {
+        path: "/signin",
+        element: <SignIn />,
+      },
+    ],
+    errorElement: <PageNotFound />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+
+root.render(
+  <Provider store={store}>
+    <RouterProvider router={appRouter} />
+  </Provider>
+);
