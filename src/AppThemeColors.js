5318542ca2fd3ea64e640ead376ccb7eef0b47fd
Art_Twis <34947496+ArtTwis@users.noreply.github.com>
2024-08-31 04:00:05 +0530

    added appThemeColor

---
+++ src/AppThemeColors.js
@@ -0,0 +1,24 @@
+const AppThemeColor = {
+  dark: {
+    bodyBgColor: "#202124",
+    primaryColor: "#33D7C3",
+    secondaryColor: "#181818",
+    textColor: "#ffffff",
+    dangerColor: "#FF3131",
+    borderColor: "#858585",
+    white: "#ffffff",
+    black: "#000000",
+  },
+  light: {
+    bodyBgColor: "#EAEBD5",
+    primaryColor: "#18776C",
+    secondaryColor: "#DCDEBA",
+    textColor: "#000000",
+    dangerColor: "#FF3131",
+    borderColor: "#444444",
+    white: "#ffffff",
+    black: "#000000",
+  },
+};
+
+export default AppThemeColor;
